b31b63b7c58d32efadb969945b237d7ead45edd2
Luca <Luca.checchin97@gmail.com>
2024-06-23 22:00:33 +0200

    finished challenge-1. Add dataset ops, running env, linear model

--- challenge1/src/logger.py
+++ challenge1/src/logger.py
@@ -1,134 +1,110 @@
 import logging
 import sys
-from pathlib import Path
+import pathlib
 from random import randint
 
-
 FORMAT = "%(asctime)s\t -\t%(name)s - [%(levelname)s] : %(message)s"
-LEVEL = logging.INFO
+LEVEL = logging.DEBUG
 
 
-class Color():
+class Color:
     GREY = "\033[38;20m"
     YELLOW = "\033[33;20m"
     BOLD_RED = "\033[31;1m"
-    RED   = "\033[1;31m"  
-    BLUE  = "\033[1;34m"
-    CYAN  = "\033[1;36m"
+    RED = "\033[1;31m"
+    BLUE = "\033[1;34m"
+    CYAN = "\033[1;36m"
     GREEN = "\033[0;32m"
     RESET = "\033[0;0m"
-    BOLD    = "\033[;1m"
+    BOLD = "\033[;1m"
     REVERSE = "\033[;7m"
-    
-# add logger session_id to filename
 
-class CustomLogger():
+
+class CustomLogger:
     """Custom logger object used to record the ML activities"""
-    
-    format = logging.Formatter(fmt=FORMAT,) #datefmt="%d-%m-%Y %H:%M:%S")
-    
-    def __init__(self, model_name : str) -> None:
-                
+
+    format = logging.Formatter(fmt=FORMAT)
+
+    def __init__(self, model_id: str) -> None:
         # define logger
-        self.logger = logging.getLogger(name=model_name)
-        
+        self.logger = logging.getLogger(name=model_id)
+
         # set minimum logger level
         self.set_logger_level()
-        
+
         # define the logger output options
-        
-            # 1. file
-        self.set_file_config(model_name=model_name)
-            
-            # 2. terminal 
+
+        # 1. file
+        self.set_file_config(model_id=model_id)
+
+        # 2. terminal
         self.set_terminal_config()
-        
-        
+
+        # Introduce new session
+        self.start_session(session_model_id=model_id)
+
     def set_logger_level(self) -> None:
         """Set the logger min level -> default : INFO"""
         self.logger.setLevel(level=LEVEL)
-        
-    
-    def set_file_config(self, model_name : str) -> None:
+
+    def set_file_config(self, model_id: str) -> None:
         """Define the file output to have persistent records. File name depends on 'model_name' parameter used to create the Class instance"""
+        # Create Log dir if it doesn't exist
+        pathlib.Path().parent.joinpath("Logs").mkdir(exist_ok=True)
+
         # Define filename
-        filename = f"Logs\{model_name}.log"
-        
+        filename = pathlib.Path().parent.joinpath("Logs", f"{model_id}.log")
+        # filename = Path.
+
         # Configure the file handler
         file_handler = logging.FileHandler(filename=filename)
         file_handler.setFormatter(fmt=CustomLogger.format)
-        
+
         # Add handler to model logger
         self.logger.addHandler(hdlr=file_handler)
-        
-    
+
     def set_terminal_config(self) -> None:
         """Define the terminal output for logs"""
-        
+
         # Configure the terminale handler
         terminal_handler = logging.StreamHandler(stream=sys.stdout)
         terminal_handler.setFormatter(CustomLogger.format)
-        
+
         # Add handler to model logger
         self.logger.addHandler(terminal_handler)
-        
-    def write_error(self, msg : str) -> None:
+
+    def write_error(self, msg: str) -> None:
         """Write 'ERROR' level logs"""
-        
+
         print(Color.RED, end="")
         self.logger.error(msg)
         print(Color.RESET, end="")
-        
+
     def write_info(self, msg: str) -> None:
         """Write 'INFO' level logs"""
-        
+
         self.logger.info(msg)
         print(Color.RESET, end="")
-        
-    
+
     def write_warning(self, msg: str) -> None:
         """Write 'WARNING' level logs"""
-        
+
         print(Color.YELLOW, end="")
         self.logger.warning(msg)
         print(Color.RESET, end="")
-    
-    def start_session(self,) -> None:
+
+    def start_session(self, session_model_id: str) -> None:
         """Log message used to initiate a new session: 'INFO' level message"""
-        
-        self.logger.info("NEW SESSION STARTING...")
-        
-    def conclude_session(self, success = False , msg = "") -> None:
-        """Log message used to terminate a session: Check whether the sessions finished correctly depending on the success parameter.
-        
-        """
-        
-        if success:
-            self.logger.info("END SESSION: SUCCESS")
+
+        self.logger.info(f"NEW SESSION STARTING : {session_model_id}")
+
+    def conclude_session(self, success=False, msg="") -> None:
+        """Log message used to terminate a session: Check whether the sessions finished correctly depending on the success parameter."""
+        if success == True:
+            self.write_info(
+                "END SESSION: SUCCESS \n---------------------------------------------------------------"
+            )
         else:
-            self.logger.error(f"SESSION TERMINATED WITH ERROR: {msg}")
-            
-        
-# if __name__ == "__main__":
-    
-#     l = CustomLogger("REGRESSION")
-    
-#     for i in range(25):
-#         k = randint(0,11) 
-#         l.write_info(k)
-#         if k % 3 == 0:
-#             l.write_error(i)
-#         elif k % 2 == 0:
-#             l.write_warning(i)
-
-            
-    
-    
-    
-    
-    
-    
-
-    
-    
-    
+            self.write_error(
+                f"SESSION TERMINATED: {msg}\n---------------------------------------------------------------"
+            )
