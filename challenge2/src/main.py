2596580818cc00626c2d1efeb9d687f58c588e14
Luca <Luca.checchin97@gmail.com>
2024-06-24 18:24:09 +0200

    feat: new model xgboost integration.

---
+++ challenge2/src/main.py
@@ -0,0 +1,51 @@
+from models import LinearRegressionModel, BoostLinearRegressionModel , SupervisedModel, AVAILABLE_MODELS
+import sys
+
+
+WEB_DATASET = "https://raw.githubusercontent.com/xtreamsrl/xtream-ai-assignment-engineer/main/datasets/diamonds/diamonds.csv"
+
+
+def get_model_instance(model_name: str) -> SupervisedModel:
+    
+    
+    if not model_name in AVAILABLE_MODELS:
+        raise Exception(f"No model named {model_name}. Available models: {AVAILABLE_MODELS}")
+    
+    if model_name == "linear":
+        return LinearRegressionModel()
+    elif model_name == "xgboost":
+        return BoostLinearRegressionModel()
+    
+    ## Add for more models
+
+
+def automated_pipeline(dataset_path: str) -> None:
+    
+    try:
+        
+        # Get input model from pipeline
+        
+        choosen_model = sys.argv[1]
+    
+        # Initiate the model class
+        model = get_model_instance(choosen_model)
+        
+        # Learning steps
+        # 1. Process dataset
+        model.data_pre_processing(dataset_path=dataset_path)
+        # # 2. Train model
+        model.train()
+        # # # 3. Create Report: Prediction -> Performance Analisys -> Model Export -> Model Session Information Export
+        model.model_report()
+        # # # # 4. End Session
+        model.model_logger.conclude_session(success=True)
+        
+    except IndexError as ie:
+        print("No model indicated. Available models:")
+        for model in AVAILABLE_MODELS: 
+            print(model)
+    except Exception as e:
+        print(e)
+
+
+automated_pipeline(WEB_DATASET)
